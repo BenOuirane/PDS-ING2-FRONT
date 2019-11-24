6388e33ae64d14ad3d8dedd43947cc6b391750cb
Justin Aguesse <justin.aguesse@etu.u-pec.fr>
2019-11-24 19:44:54 +0100

    Create Service Component

--- src/app/company.service.ts
+++ src/app/company.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { Company } from './company';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CompanyService {
+    private baseUrl = 'http://localhost:8080/api/company';
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    createCompany(company: Company): Observable<Object> {
+
+        return this.http.post(`${this.baseUrl}` + `/create`, company); 
+
+    }
+
+    getUserList(): Observable<any> {
+        return this.http.get(`${this.baseUrl}`);
+    }
+  
 }
