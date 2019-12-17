6019220485322fe3b731e6af2cefda41fa1d24d5
Ismail El Hammoud <ismail16051999@gmail.com>
2019-12-17 19:50:25 +0100

    updating branch commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,91 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {User} from "./user";
+import { timer } from 'rxjs';
+import { User } from "./user";
+import { Notification } from "./notification"
+import { NotificationService } from "./notification.service";
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-/*
-Here, we give some attributes to the html page
-(app.component.html),
-we can call them with {xxx} on the html code
- */
 export class AppComponent {
-  title = 'Application Angular FrontEnd';
-  description = 'Ceci est un test d\'application FrontEnd en Angular comminiquant avec un BackEnd en Spring.';
 
   user: User = new User();
+  numberNotification: number;
+  newNotifications: Notification[];
+  notificationsString: string;
 
+  constructor(private router: Router, private notificationService: NotificationService) {
+    this.numberNotification = 0;
+  }
 
-  constructor(private router: Router) {}
+  ngOnInit() {
+    this.toggleHeaderStyle();
+
+    if(!window.location.href.includes('login')) {
+
+      this.user = JSON.parse(localStorage.getItem('user'));
+      if(this.user.role == 'RESIDENT'){
+
+        timer(0, 10000).subscribe(() => {
+          this.numberNotification = 0;
+          this.notificationService.getNotification(this.user.id).subscribe(
+            data => {
+              this.notificationsString = JSON.stringify(data);
+              this.newNotifications = JSON.parse(this.notificationsString);
+              this.newNotifications.forEach(notification => {
+                if (notification.state == "PENDING") {
+                  this.numberNotification = this.numberNotification + 1;
+                }
+              });
+            },
+            error => {
+              console.log(error);
+            }
+          );
+        });
+
+      }
+    }
+    
+  }
+
+  toggleHeaderStyle() {
+    if ($(window).scrollTop() > 64) {
+        $('#navbarBox').css("height", "80px");
+    } else {
+        $('#navbarBox').css("height", "100px");
+    }
+
+    if ($(window).width() < 768) {
+        if(!$('.navbar-collapse').hasClass("collapsed-menu")) {
+            $('.navbar-collapse').addClass("collapsed-menu");
+        }
+    } else {
+        if($('.navbar-collapse').hasClass("collapsed-menu")) {
+            $('.navbar-collapse').removeClass("collapsed-menu");
+        }
+    }
+  }
+
+  showMenu() {
+    $('#navbar').toggleClass("show");
+  }
 
   logout() {
     localStorage.removeItem('user');
-    this.router.navigate(['login']);
+    window.location.href = "/login";
   }
-  
 
   authenticated() {
     this.user = JSON.parse(localStorage.getItem('user'));
     return localStorage.getItem('user');
   }
+
+  
 }
+
