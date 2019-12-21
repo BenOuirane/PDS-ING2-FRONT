eb1bc4379a729f5aa2abe0d311217f26d0f6b9c4
JAguesse <jaguesse@outlook.fr>
2019-12-21 16:55:05 +0100

    Build services to get lampes info.
    No component front created

---
+++ src/app/lampe.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Objects } from './objects'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LampeService {
+
+  //private baseUrl = 'http://localhost:8080/api';
+  private baseUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getNotification(objects: Objects){
+    return this.http.put(`${this.baseUrl}` + `/lamp/list`, objects);
+  }
+
+}
---
+++ src/app/resident.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { User } from "./user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResidentService {
+
+  //private baseUrl = 'http://localhost:8080/api';
+  private baseUrl = 'http://localhost:8080/api';
+  
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getResident(user: User){
+    return this.http.put(`${this.baseUrl}` + `//Resident/singleton`, user);
+  }
+
+}
