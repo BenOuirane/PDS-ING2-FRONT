1a36cd2ccfd38eaf5b064111cffed97bada3a740
JAguesse <jaguesse@outlook.fr>
2019-12-22 14:54:38 +0100

    Add routing to field "Mes Objets connectés"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from "./helpers/AuthGuard";
 import { RoleGuard } from './helpers/RoleGuard';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { UpdateDatamockComponent } from './update-datamock/update-datamock.component';
+import { ObjectComponent } from './object/object.component'; 
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'notification', component: SendNotificationComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN'}},
   { path: 'notifications', component: GetNotificationsComponent, canActivate: [RoleGuard], data: { expectedRole: 'RESIDENT'}},
+  { path: 'object', component: ObjectComponent, canActivate: [RoleGuard], data: {expectedRole: 'RESIDENT'}},
   { path: 'login', component: LoginComponent },
   { path: 'search', component: SearchBarComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN'}},
   { path: 'updatedata', component: UpdateDatamockComponent, canActivate: [RoleGuard], data: { expectedRole: 'ADMIN'}}
