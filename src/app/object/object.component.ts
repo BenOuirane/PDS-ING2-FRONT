1515a47721e2ff4cfcce791ede0942373c0f6a8b
JAguesse <jaguesse@outlook.fr>
2020-02-03 15:43:40 +0100

    param Lamp V1 focntionnel
    Time function defective but in a good way.

--- src/app/object/object.component.ts
+++ src/app/object/object.component.ts
@@ -27,89 +27,113 @@ registerLocaleData(localeFr, 'fr');
 })
 export class ObjectComponent implements OnInit {
 
-  user : User = new User(); 
-  room : Room = new Room(); 
-  roomString : string; 
-  objects : Objects[] = new Array<Objects>();
-  lamps : Lampe[] = new Array<Lampe>();
-  ovens : Oven[] = new Array<Oven>();
-  alarmClocks : AlarmClock[] = new Array<AlarmClock>();
-  coffeeMachines : CoffeeMachine[] = new Array<CoffeeMachine>();
-  shutters : Shutter[] = new Array<Shutter>();
-  dataloaded : boolean = false;
-  LampStatus : boolean = false;
+  user: User = new User();
+  room: Room = new Room();
+  roomString: string;
+  objects: Objects[] = new Array<Objects>();
+  lamps: Lampe[] = new Array<Lampe>();
+  ovens: Oven[] = new Array<Oven>();
+  alarmClocks: AlarmClock[] = new Array<AlarmClock>();
+  coffeeMachines: CoffeeMachine[] = new Array<CoffeeMachine>();
+  shutters: Shutter[] = new Array<Shutter>();
+  dataloaded: boolean = false;
+  LampStatus: boolean = false;
+  
 
 
 
 
-  constructor(private residentService: ResidentService, private objectService: ObjectService, private lampeService : LampeService, private ovenService : OvenService, private shutterService : ShutterService, private alarmClockService : AlarmClockService, private coffeeMachineService : CoffeeMachineService) { }
+  constructor(private residentService: ResidentService, private objectService: ObjectService, private lampeService: LampeService, private ovenService: OvenService, private shutterService: ShutterService, private alarmClockService: AlarmClockService, private coffeeMachineService: CoffeeMachineService) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('user'));
 
     this.residentService.getResident(this.user).subscribe(
       data => {
-        this.room = data.room; 
+        this.room = data.room;
         this.objects = this.room.objects;
-        
-        this.objects.forEach(object =>
-          {
-            switch(object.objectType){
-                case 'LAMP' : 
-                  this.lampeService.getlampe(object).subscribe(
-                    data => {
-                      this.lamps = data;
-                      console.log(data);
-                    }
-                  ); break; 
-                case 'OVEN' : 
-                this.ovenService.getOven(object).subscribe(
-                  data => {
-                    this.ovens = data; 
-                    console.log(data);
-                  }
-                ); break; 
 
-                case 'SHUTTER' : 
-                  this.shutterService.getshutter(object).subscribe(
-                    data => {
-                      this.shutters = data; 
-                      console.log(data);
-                    } 
-                  ); break;
+        this.objects.forEach(object => {
+          switch (object.objectType) {
+            case 'LAMP':
+              this.lampeService.getlampe(object).subscribe(
+                data => {
+                  this.lamps = data;
+                  console.log(data);
+                }
+              ); break;
+            case 'OVEN':
+              this.ovenService.getOven(object).subscribe(
+                data => {
+                  this.ovens = data;
+                  console.log(data);
+                }
+              ); break;
 
-                case 'ALARMCLOCK' : 
-                    this.alarmClockService.getAlarmClock(object).subscribe(
-                      data => {
-                        this.alarmClocks = data; 
-                        console.log(data);
-                      }
-                    ); break; 
-                
-                case 'COFFEEMACHINE' : 
-                this.coffeeMachineService.getCoffeeMachine(object).subscribe(
-                  data => {
-                    this.coffeeMachines = data; 
-                    console.log(data);
-                  }
-                ); break; 
+            case 'SHUTTER':
+              this.shutterService.getshutter(object).subscribe(
+                data => {
+                  this.shutters = data;
+                  console.log(data);
+                }
+              ); break;
 
+            case 'ALARMCLOCK':
+              this.alarmClockService.getAlarmClock(object).subscribe(
+                data => {
+                  this.alarmClocks = data;
+                  console.log(data);
+                }
+              ); break;
 
-                default : 
-                break;
-            }
-            this.dataloaded = true;
-          })
-
-      },  error => console.log(error)
-      );
-
-      
+            case 'COFFEEMACHINE':
+              this.coffeeMachineService.getCoffeeMachine(object).subscribe(
+                data => {
+                  this.coffeeMachines = data;
+                  console.log(data);
+                }
+              ); break;
 
 
+            default:
+              break;
+          }
+          this.dataloaded = true;
+        })
 
-    
-    
+      }, error => console.log(error)
+    );
+  }
+ 
+  //Used to get automaticaly the right color 
+  colorOnChange(value: string) : string {
+    let color: string = ''; 
+    switch (value) {
+      case 'WHITE':
+          color = 'Blanche'
+        break;
+      case 'BLUE':
+          color = 'Bleue'
+        break;
+        case 'GREEN':
+          color = 'Verte'
+        break;
+        case 'YELLOW':
+          color = 'Jaune'
+        break;
+
+      default:
+        break;
+
+    }
+    return color;
   }
 
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
 }
