963ca96e054ff9e4a4b80ab9ec9d529b69a442da
JAguesse <jaguesse@outlook.fr>
2020-02-03 11:46:18 +0100

    Update URL in local for dev

--- src/app/oven.service.ts
+++ src/app/oven.service.ts
@@ -10,8 +10,8 @@ import { Oven } from './oven';
 })
 export class OvenService {
 
-  //private baseUrl = 'http://localhost:8080/api';
-  private baseUrl = 'http://172.31.254.61:8080/api';
+  private baseUrl = 'http://localhost:8080/api';
+  //private baseUrl = 'http://172.31.254.61:8080/api';
 
   constructor(private http: HttpClient, private router: Router) { }
 
