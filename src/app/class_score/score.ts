a9c63b9bde5bf8a91ff0bb319ba76da2b1104112
BenOuirane <33792892+BenOuirane@users.noreply.github.com>
2020-05-15 15:08:18 +0200

    create class score

---
+++ src/app/class_score/score.ts
@@ -0,0 +1,19 @@
+export class Score {
+    id:number ;
+    note_no_athletic:number ;
+    note_athletic:number ;
+    note_good_financial_condition:number ;
+    note_bad_financial_condition:number ;
+    note_self_ruling:number ;
+    note_no_self_ruling:number ;
+    note_good_social_condition:number ;
+    note_bad_social_condition:number ;
+    note_sociable:number ;
+    note_no_sociable:number ;
+    note_like_cooking:number ;
+    note_like_rest:number ;
+    note_like_game:number;
+    note_not_sick :number;
+    note_hypertension:number ;
+    note_diabetes:number ;
+}
