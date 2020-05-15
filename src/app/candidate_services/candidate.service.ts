1b0f67c292e10500ba33f2c7f31a9ff6f4aa7678
BenOuirane <33792892+BenOuirane@users.noreply.github.com>
2020-05-15 21:10:29 +0200

    confir uri

--- src/app/candidate_services/candidate.service.ts
+++ src/app/candidate_services/candidate.service.ts
@@ -7,10 +7,10 @@ import { Candidate } from '../class_candidate/candidate';
 @Injectable()
 export class CandidateService {
 
- // private baseUrl:string= "http://localhost:8080/candidates/create";
-  private baseUrl:string= "http://172.31.254.61:8080/candidates/create";
+  //private baseUrl:string= "http://localhost:8080/candidates/create";
+   private baseUrl:string= "http://172.31.254.61:8080/candidates/create";
   
- // Url= "http://localhost:8080/score";
+//  Url= "http://localhost:8080/score";
  Url = "http://172.31.254.61:8080/score";
  // Urlp= "http://localhost:8080/sort";
   Urlp = "http://172.31.254.61:8080/sort";
